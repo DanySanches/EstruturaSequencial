e6b7a14166e8cfac1039212630be50edd7ee02fd
danysanches@msn.com <danysanches@msn.com>
2024-09-11 22:21:22 -0300

    Exercício 16 finalizado

--- src/main/java/com/Dani/ExerciciosEstruturaSequencial/Exercicio16.java
+++ src/main/java/com/Dani/ExerciciosEstruturaSequencial/Exercicio16.java
@@ -8,12 +8,50 @@ package com.Dani.ExerciciosEstruturaSequencial;
 //c)quanto pagou ao sindicato.
 //d)o salário líquido.
 //e)calcule os descontos e o salário líquido, conforme a tabela abaixo:
-
 //+ Salário Bruto : R$
 //- IR (11%) : R$
 //- INSS (8%) : R$
 //- Sindicato ( 5%) : R$
 //= Salário Liquido : R$
 //Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+import java.util.Locale;
+import java.util.Scanner;
 public class Exercicio16 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor da hora trabalhada: ");
+        double hora = sc.nextDouble();
+
+        System.out.println("Digite quantas horas trabalhadas: ");
+        int horasTrabalhadas = sc.nextInt();
+
+        double salarioBruto = hora * horasTrabalhadas;
+
+        //11%
+        double impostoRenda = salarioBruto * 0.11;
+
+        //8%
+        double inss = salarioBruto * 0.08;
+
+        //5%
+        double sindicato = salarioBruto * 0.05;
+
+        double salarioLiquido = salarioBruto - impostoRenda - inss - sindicato;
+
+
+        System.out.printf("Salario Bruto: R$ %.2f \n", salarioBruto);
+        System.out.printf("IR :         - R$ %.2f\n", impostoRenda);
+        System.out.printf("INSS :       - R$ %.2f\n", inss);
+        System.out.printf("Sindicato:   - R$ %.2f\n", sindicato);
+        System.out.println("__________________________");
+        System.out.printf("Total:         R$ %.2f ", salarioLiquido);
+
+        sc.close();
+
+    }
 }
